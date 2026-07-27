00113cfc9abc99cdc2ba4f9fb81578a122aa2bfc
Josep Comes <jcomes@jcomes.org>
2026-07-27 13:05:21 +0000

    sway: gruvbox mako config (per-urgency color/opacity, 5s auto-close)

--- install_env.sh
+++ install_env.sh
@@ -527,6 +527,7 @@ installSway() {
 	cp -r "$REPO_DIR/dotfiles/.config/sway" "$HOME/.config/"
 	cp -r "$REPO_DIR/dotfiles/.config/waybar" "$HOME/.config/"
 	cp -r "$REPO_DIR/dotfiles/.config/gtklock" "$HOME/.config/"
+	cp -r "$REPO_DIR/dotfiles/.config/mako" "$HOME/.config/"
 	cp -r "$REPO_DIR/dotfiles/.config/xdg-desktop-portal" "$HOME/.config/"
 	cp -r "$REPO_DIR/dotfiles/.config/environment.d" "$HOME/.config/"
 	# alacritty is cargo-installed (~/.cargo/bin), which a display-manager-launched
